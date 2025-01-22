72ccaf03e49fdff154c1280a7d8d9a98804fe1e3
mariappan0301 <mariappanr2004@gmail.com>
2025-01-22 22:58:56 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Image1 from "../../assets/hero/plumbing-tool.png";
+import Image2 from "../../assets/hero/gold-faucet.png";
+import Image3 from "../../assets/hero/construction-pipes.png";
+import Slider from "react-slick";
+
+const ImageList = [
+  {
+    id: 1,
+    img: Image1,
+    title: "Upto 40% off on all Tools Materials",
+    description:
+      "Tackling a leaky faucet, installing new pipes, or performing routine maintenance, our tools ensure precision, durability, and ease of use.",
+  },
+  {
+    id: 2,
+    img: Image2,
+    title: "30% off on all Water Faucets",
+    description:
+      "Designed with advanced features that improve water conservation.",
+  },
+  {
+    id: 3,
+    img: Image3,
+    title: "20% off on all PVC Pipes",
+    description:
+      "PVC pipes for a variety of purposes, including water and wastewater conveyance, electrical wiring, and plumbing.",
+  },
+];
+
+const Hero = ({ handleOrderPopup }) => {
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
+
+  return (
+    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-slate-950 dark:text-white duration-200 ">
+      {/* background pattern */}
+      <div className="h-[700px] w-[700px] bg-secondary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]"></div>
+      {/* hero section */}
+      <div className="container pb-8 sm:pb-0">
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                  <h1
+                    data-aos="zoom-out "
+                    data-aos-duration="500"
+                    data-aos-once="true"
+                    className="text-5xl sm:text-6xl lg:text-7xl font-bold"
+                  >
+                    {data.title}
+                  </h1>
+                  <p
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="100"
+                    className="text-sm"
+                  >
+                    {data.description}
+                  </p>
+                  <div
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="300"
+                  >
+                    <button
+                      onClick={handleOrderPopup}
+                      className="bg-gradient-to-r from-secondary to-primary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full"
+                    >
+                      Order Now
+                    </button>
+                  </div>
+                </div>
+                {/* image section */}
+                <div className="order-1 sm:order-2">
+                  <div
+                    data-aos="zoom-in"
+                    data-aos-once="true"
+                    className="relative z-10"
+                  >
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
